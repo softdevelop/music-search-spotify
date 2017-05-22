4f40af9acf0df12554960cebc494967ff8b4d9e1
duongcybersmart <duong@cybersmart.co.uk>
2017-05-22 09:16:32 +0700

    Create code

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var app = angular.module('App', [
+    'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        // Home
+        .when("/", { templateUrl: "partials/home.html", controller: "HomeCtrl" })
+        // else 404
+        .otherwise("/404", { templateUrl: "partials/404.html", controller: "ErrorCtrl" });
+}]);
+
+app.controller('HomeCtrl', function( /* $scope, $location, $http */ ) {
+    console.log("Page Controller reporting for duty.");
+
+});
+
+app.controller('ErrorCtrl', function( /* $scope, $location, $http */ ) {
+
+});
